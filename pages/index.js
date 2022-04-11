cd16b63c3f2b853c32c5dedb6dee75cbb7432860
Rajaraj1 <rr702678@gmail.com>
2022-04-12 01:06:28 +0530

    fourth

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,7 @@ const index = () => {
   return (
     <>
       <h1 className="w-[200px] mx-auto font-bold text-2xl my-8">
-<<<<<<< HEAD
         This is Raja raj
-=======
-        This is Riya raj
->>>>>>> 90c7a779dda7c718c560b99610c832d2090a8ee0
       </h1>
     </>
   );
