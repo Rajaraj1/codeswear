cb0c88919aa04d090bbfab686675fd22df8bd84a
Rajaraj1 <rr702678@gmail.com>
2022-04-12 01:04:42 +0530

    fourth

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,10 @@ import React from "react";
 const index = () => {
   return (
     <>
-      < className="w-[200px] mx-auto font-bold text-2xl my-8">
+      <h1 className="w-[200px] mx-auto font-bold text-2xl my-8">
         This is Raja raj
-      </>
+      </h1>
+    </>
   );
 };
 
