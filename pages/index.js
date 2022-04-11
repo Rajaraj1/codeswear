05dc59f33e1566ae35eab4629865f0a1ab3ee25d
Rajaraj1 <rr702678@gmail.com>
2022-04-12 00:35:54 +0530

    Second

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const index = () => {
-  return <></>;
+  return (
+    <>
+      <h1 className="w-[200px] mx-auto font-bold text-2xl my-8">
+        This is Raja raj
+      </h1>
+    </>
+  );
 };
 
 export default index;
